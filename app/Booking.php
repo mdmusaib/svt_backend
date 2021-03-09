1299bbaf435c5cc918b757d7de254503560cec52
Musaib <musaibkm@gmail.com>
2021-03-09 16:10:48 +0530

    Update Booking.php

--- app/Booking.php
+++ app/Booking.php
@@ -9,7 +9,7 @@ class Booking extends Model
     protected $fillable = [
 
     	'customer_type','sender_mobile','sender_origin','receiver_name','receiver_mobile','receiver_address',
-        'items','total_weight','rate','date','total_amount'
+        'items','total_weight','rate','date','total_amount','tax'
 
     ];
 }
